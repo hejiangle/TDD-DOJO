78d984817444966715e3d06a5ad231432a6b7c7a
hejiangle <jlhe@thoughtworks.com>
2019-05-21 22:36:35 +0800

    Bridge-5 Add Double pairs hand cards UT cases and failds.

--- Bridge.Tests/UmpireTests.cs
+++ Bridge.Tests/UmpireTests.cs
@@ -9,18 +9,9 @@ namespace Bridge.Tests
     {
         [Theory]
         [MemberData(nameof(MessyHandCards))]
-        public void ShouldCompareTwoHandsMessyCardsAndReturnTheHighestCard(List<string> hardCards_1, List<string> hardCards_2, string expected)
-        {
-            var umpire = new Umpire();
-
-            var theHighestCard = umpire.CompareCards(hardCards_1, hardCards_2);
-            
-            Assert.Equal(expected, theHighestCard);
-        }
-        
-        [Theory]
         [MemberData(nameof(OnePairHandCards))]
-        public void ShouldCompareTwoHandsOnePairCardsAndReturnTheHighestCard(List<string> hardCards_1, List<string> hardCards_2, string expected)
+        [MemberData(nameof(DoublePairsHandCards))]
+        public void ShouldCompareTwoHandsCardsAndReturnTheHighestCard(List<string> hardCards_1, List<string> hardCards_2, string expected)
         {
             var umpire = new Umpire();
 
@@ -35,12 +26,19 @@ namespace Bridge.Tests
             new object[]{new List<string> { "2H", "3D", "5S", "9C", "QD"}, new List<string> {"2C", "3H", "4S", "8C", "TH"}, "Q"},
             new object[]{new List<string> { "2H", "3D", "5S", "9C", "QD"}, new List<string> {"2C", "3H", "4S", "8C", "QH"}, "9"},
         };
-        
+
         public static IEnumerable<object[]> OnePairHandCards => new List<object[]>
         {
             new object[]{new List<string> { "2H", "2D", "5S", "9C", "KD"}, new List<string> {"3C", "3H", "4S", "8C", "AH"}, "3"},
             new object[]{new List<string> { "2H", "3D", "9S", "9C", "QD"}, new List<string> {"2C", "3H", "4S", "TC", "TH"}, "T"},
             new object[]{new List<string> { "2H", "3D", "5S", "QC", "QD"}, new List<string> {"2C", "3H", "4S", "QC", "QH"}, "5"},
+        };
+
+        public static IEnumerable<object[]> DoublePairsHandCards => new List<object[]>
+        {
+            new object[]{new List<string> { "2H", "2D", "5S", "5C", "KD"}, new List<string> {"3C", "3H", "4S", "4C", "AH"}, "5"},
+            new object[]{new List<string> { "2H", "2D", "9S", "9C", "QD"}, new List<string> {"2C", "4H", "4S", "TC", "TH"}, "T"},
+            new object[]{new List<string> { "2H", "2D", "5S", "QC", "QD"}, new List<string> {"2C", "2H", "4S", "QC", "QH"}, "5"},
         };
     }
 }
