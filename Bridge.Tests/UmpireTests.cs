2a304a691ce2712d5571077dd5602b4c735939e4
hejiangle <jlhe@thoughtworks.com>
2019-05-21 23:50:00 +0800

    Bridge-6 Pass the test cases and add 'Tie' cases.

--- Bridge.Tests/UmpireTests.cs
+++ Bridge.Tests/UmpireTests.cs
@@ -25,6 +25,7 @@ namespace Bridge.Tests
             new object[]{new List<string> { "2H", "3D", "5S", "9C", "KD"}, new List<string> {"2C", "3H", "4S", "8C", "AH"}, "A"},
             new object[]{new List<string> { "2H", "3D", "5S", "9C", "QD"}, new List<string> {"2C", "3H", "4S", "8C", "TH"}, "Q"},
             new object[]{new List<string> { "2H", "3D", "5S", "9C", "QD"}, new List<string> {"2C", "3H", "4S", "8C", "QH"}, "9"},
+            new object[]{new List<string> { "2H", "3D", "5S", "9C", "QD"}, new List<string> {"2C", "3H", "5S", "9C", "QH"}, "Tie"},
         };
 
         public static IEnumerable<object[]> OnePairHandCards => new List<object[]>
@@ -32,6 +33,7 @@ namespace Bridge.Tests
             new object[]{new List<string> { "2H", "2D", "5S", "9C", "KD"}, new List<string> {"3C", "3H", "4S", "8C", "AH"}, "3"},
             new object[]{new List<string> { "2H", "3D", "9S", "9C", "QD"}, new List<string> {"2C", "3H", "4S", "TC", "TH"}, "T"},
             new object[]{new List<string> { "2H", "3D", "5S", "QC", "QD"}, new List<string> {"2C", "3H", "4S", "QC", "QH"}, "5"},
+            new object[]{new List<string> { "2H", "3D", "5S", "QC", "QD"}, new List<string> {"2C", "3H", "5S", "QC", "QH"}, "Tie"},
         };
 
         public static IEnumerable<object[]> DoublePairsHandCards => new List<object[]>
@@ -39,6 +41,7 @@ namespace Bridge.Tests
             new object[]{new List<string> { "2H", "2D", "5S", "5C", "KD"}, new List<string> {"3C", "3H", "4S", "4C", "AH"}, "5"},
             new object[]{new List<string> { "2H", "2D", "9S", "9C", "QD"}, new List<string> {"2C", "4H", "4S", "TC", "TH"}, "T"},
             new object[]{new List<string> { "2H", "2D", "5S", "QC", "QD"}, new List<string> {"2C", "2H", "4S", "QC", "QH"}, "5"},
+            new object[]{new List<string> { "2H", "2D", "5S", "QC", "QD"}, new List<string> {"2C", "2H", "5S", "QC", "QH"}, "Tie"},
         };
     }
 }
--- Bridge/Umpire.cs
+++ Bridge/Umpire.cs
@@ -15,54 +15,115 @@ namespace Bridge
             var whiteCards = whiteHandCards.Select(Card.Parse).OrderByDescending(card => card.Value).ToList();
             var blackCards = blackHandCards.Select(Card.Parse).OrderByDescending(card => card.Value).ToList();
             
-            var highCard = string.Empty;
-            if (IsMessyCards(whiteCards) && IsMessyCards(blackCards))
+            if (IsDoublePairsCards(whiteCards) && IsDoublePairsCards(blackCards))
             {
-                highCard = CompareMessyCards(whiteCards, blackCards, 5);
+                return CompareDoublePairsHandCards(whiteCards, blackCards);
             }
-
-            if (IsOnePairCards(whiteCards) && IsMessyCards(blackCards))
+            if (IsDoublePairsCards(whiteCards) || IsDoublePairsCards(blackCards))
             {
-                highCard = string.Format(WHITE_WIN_TEMPLATE, "Pair");
-            }
-
-            if (IsMessyCards(whiteCards) && IsOnePairCards(blackCards))
-            {
-                highCard = string.Format(BLACK_WIN_TEMPLATE, "Pair");
+                return DoublePairsWin(whiteCards, blackCards);
             }
 
             if (IsOnePairCards(whiteCards) && IsOnePairCards(blackCards))
             {
-                var whitePair = whiteCards.Find(x => whiteCards.Count(y => y.Number.Equals(x.Number)) == 2);
-                var blackPair = blackCards.Find(x => blackCards.Count(y => y.Number.Equals(x.Number)) == 2);
+                return CompareOnePairHandCards(whiteCards, blackCards);
+            }
+            if (IsOnePairCards(whiteCards) || IsOnePairCards(blackCards))
+            {
+                return OnePairWin(whiteCards, blackCards);
+            }
 
-                var compareResult = whitePair.CompareTo(blackPair);
+            return CompareMessyCards(whiteCards, blackCards, 5);
+        }
 
-                if (compareResult > 0)
-                {
-                    highCard = whitePair.Number.ToString();
-                }
+        private string CompareDoublePairsHandCards(List<Card> whiteCards, List<Card> blackCards)
+        {
+            var whitePairs = whiteCards
+                .Where(x => whiteCards.Count(y => y.Number.Equals(x.Number)) == 2)
+                .Distinct()
+                .ToList();
+            var blackPairs = blackCards
+                .Where(x => blackCards.Count(y => y.Number.Equals(x.Number)) == 2)
+                .Distinct()
+                .ToList();
 
-                if (compareResult < 0)
-                {
-                    highCard = blackPair.Number.ToString();
-                }
+            var highCard = CompareMessyCards(whitePairs, blackPairs, 2);
 
-                if (compareResult == 0)
-                {
-                    whiteCards.RemoveAll(card => card.Equals(whitePair));
-                    blackCards.RemoveAll(card => card.Equals(blackPair));
-                    highCard = CompareMessyCards(whiteCards, blackCards, 3);
-                }
+            if (highCard.Equals("Tie"))
+            {
+                var singleWhiteCard = whiteCards.Where(x => !whitePairs.Exists(x.Equals)).ToList();
+                var singleBlackCard = blackCards.Where(x => !blackPairs.Exists(x.Equals)).ToList();
+
+                highCard = CompareMessyCards(singleWhiteCard, singleBlackCard, 1);
             }
 
             return highCard;
         }
 
+        private string DoublePairsWin(List<Card> whiteCards, List<Card> blackCards)
+        {
+            if (IsDoublePairsCards(whiteCards) && IsMessyCards(blackCards)
+                || IsDoublePairsCards(whiteCards) && IsOnePairCards(blackCards))
+            {
+                return string.Format(WHITE_WIN_TEMPLATE, "Double Pair");
+            }
+
+            if (IsMessyCards(whiteCards) && IsDoublePairsCards(blackCards)
+                || IsOnePairCards(whiteCards) && IsDoublePairsCards(blackCards))
+            {
+                 return string.Format(BLACK_WIN_TEMPLATE, "Double Pair");
+            }
+
+            return "Tie";
+        }
+
+        private static string CompareOnePairHandCards(List<Card> whiteCards, List<Card> blackCards)
+        {
+            var whitePair = whiteCards.Find(x => whiteCards.Count(y => y.Number.Equals(x.Number)) == 2);
+            var blackPair = blackCards.Find(x => blackCards.Count(y => y.Number.Equals(x.Number)) == 2);
+
+            var compareResult = whitePair.CompareTo(blackPair);
+            var highCard = "Tie";
+
+            if (compareResult > 0)
+            {
+                highCard = whitePair.Number.ToString();
+            }
+
+            if (compareResult < 0)
+            {
+                highCard = blackPair.Number.ToString();
+            }
+
+            if (compareResult == 0)
+            {
+                whiteCards.RemoveAll(card => card.Equals(whitePair));
+                blackCards.RemoveAll(card => card.Equals(blackPair));
+                highCard = CompareMessyCards(whiteCards, blackCards, 3);
+            }
+
+            return highCard;
+        }
+
+        private string OnePairWin(List<Card> whiteCards, List<Card> blackCards)
+        {
+            if (IsOnePairCards(whiteCards) && IsMessyCards(blackCards))
+            {
+                return string.Format(WHITE_WIN_TEMPLATE, "Pair");
+            }
+
+            if (IsMessyCards(whiteCards) && IsOnePairCards(blackCards))
+            {
+                return string.Format(BLACK_WIN_TEMPLATE, "Pair");
+            }
+
+            return "Tie";
+        }
+
         private static string CompareMessyCards(List<Card> whiteCards, List<Card> blackCards, int cardCounts)
         {
             var index = 0;
-            var highCard = string.Empty;
+            var highCard = "Tie";
             
             while (index < cardCounts)
             {
@@ -88,12 +149,30 @@ namespace Bridge
 
         private bool IsMessyCards(List<Card> hardCards)
         {
-            return !IsOnePairCards(hardCards);
+            return !IsOnePairCards(hardCards) && !IsDoublePairsCards(hardCards);
         }
 
         private bool IsOnePairCards(List<Card> hardCards)
         {
             return hardCards.Distinct().Count() == 4;
+        }
+
+        private bool IsDoublePairsCards(List<Card> hardCards)
+        {
+            var index = 0;
+            var pairsCount = 0;
+            while (index < 4)
+            {
+                if (hardCards[index].Equals(hardCards[index + 1]))
+                {
+                    pairsCount++;
+                    index++;
+                }
+
+                index++;
+            }
+
+            return pairsCount == 2 && hardCards.Distinct().Count() == 3;
         }
     }
 }
