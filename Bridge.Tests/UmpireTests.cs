531beebdb7326e00dfdae0c828be1a30e03dba07
hejiangle <jlhe@thoughtworks.com>
2019-05-22 13:24:02 +0800

    Bridge-15 Add some failed test cases for Full house type.

--- Bridge.Tests/UmpireTests.cs
+++ Bridge.Tests/UmpireTests.cs
@@ -12,6 +12,7 @@ namespace Bridge.Tests
         [MemberData(nameof(ThreeOfAKindHandCards))]
         [MemberData(nameof(StraightHandCards))]
         [MemberData(nameof(FlushHandCards))]
+        [MemberData(nameof(FullHouseHandCards))]
         [MemberData(nameof(FourOfAKindHandCards))]
         public void ShouldCompareTwoHandsCardsAndReturnTheHighestCard(List<string> hardCards_1, List<string> hardCards_2, string expected)
         {
@@ -79,6 +80,20 @@ namespace Bridge.Tests
             new object[]{new List<string> { "TH", "JD", "QS", "KC", "AD"}, new List<string> {"5H", "6H", "7H", "8H", "TH"}, "Black wins - Flush"},
         };
 
+        public static IEnumerable<object[]> FullHouseHandCards => new List<object[]>
+        {
+            new object[]{new List<string> { "2C", "2H", "2S", "9D", "9H"}, new List<string> {"8C", "8H", "8D", "AS", "AH"}, "8"},
+            new object[]{new List<string> { "2D", "2S", "2H", "8C", "8D"}, new List<string> {"2H", "2D", "2S", "6C", "6H"}, "Cheat!!!"},
+            new object[]{new List<string> { "9D", "9S", "9H", "KC", "KS"}, new List<string> {"QC", "QS", "QH", "8D", "8C"}, "Q"},
+            new object[]{new List<string> { "2C", "2H", "2D", "7S", "7H"}, new List<string> {"2C", "2H", "2D", "7S", "7S"}, "Cheat!!!"},
+            new object[]{new List<string> { "2H", "2D", "2S", "TC", "QD"}, new List<string> {"8C", "8H", "8D", "AS", "AH"}, "Black wins - FullHouse"},
+            new object[]{new List<string> { "2H", "2D", "TS", "TC", "QD"}, new List<string> {"8C", "8H", "8D", "AS", "AH"}, "Black wins - FullHouse"},
+            new object[]{new List<string> { "2H", "2D", "9S", "TC", "QD"}, new List<string> {"8C", "8H", "8D", "AS", "AH"}, "Black wins - FullHouse"},
+            new object[]{new List<string> { "AH", "2D", "9S", "TC", "QD"}, new List<string> {"8C", "8H", "8D", "AS", "AH"}, "Black wins - FullHouse"},
+            new object[]{new List<string> { "5H", "6H", "7H", "8H", "TH"}, new List<string> {"8C", "8H", "8D", "AS", "AH"}, "Black wins - FullHouse"},
+            new object[]{new List<string> { "TH", "JD", "QS", "KC", "AD"}, new List<string> {"8C", "8H", "8D", "AS", "AH"}, "Black wins - FullHouse"}
+        };
+
         public static IEnumerable<object[]> FourOfAKindHandCards => new List<object[]>
         {
             new object[]{new List<string> { "2C", "2H", "2S", "2D", "9H"}, new List<string> {"8C", "8H", "8D", "8S", "AH"}, "8"},
@@ -91,6 +106,8 @@ namespace Bridge.Tests
             new object[]{new List<string> { "AH", "2D", "9S", "TC", "QD"}, new List<string> {"8C", "8H", "8D", "8S", "AH"}, "Black wins - Four of a Kind"},
             new object[]{new List<string> { "5H", "6H", "7H", "8H", "TH"}, new List<string> {"8C", "8H", "8D", "8S", "AH"}, "Black wins - Four of a Kind"},
             new object[]{new List<string> { "TH", "JD", "QS", "KC", "AD"}, new List<string> {"8C", "8H", "8D", "8S", "AH"}, "Black wins - Four of a Kind"},
+            new object[]{new List<string> { "8C", "8H", "8D", "AS", "AH"}, new List<string> {"8C", "8H", "8D", "8S", "AH"}, "Black wins - Four of a Kind"},
+
         };
     }
 }
